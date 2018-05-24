451c9f0af74ea5e44bcee7f3a776ad984d86463f
marcusMorck <marcus.morck@gmail.com>
2018-05-24 13:18:17 +0200

    added cart

---
+++ client/client.js
@@ -0,0 +1,23 @@
+Vue.component('cart-item', CartItemComponent);
+Vue.component('cart', CartComponent);
+
+const http = axios; // using axios 3rd party XHR/REST lib
+
+// the app
+let app = new Vue({
+  el: "#app",
+  created(){
+    // ladda in litta data
+    http.get('/rest/products/').then(response => {
+      console.log('products', response.data)
+      this.products = response.data;
+    }).catch(e => {
+      console.error(e);
+    });
+  },
+  data(){
+    return {
+      products: []
+    }
+  }
+});
--- server/server.js
+++ server/server.js
@@ -9,16 +9,33 @@ app.use(bodyParser.json());
 //Serve frontend files
 app.use(express.static('../client/'));
 
+
 const AccessManager = require('access-manager');
 const accessManager = new AccessManager({
   mongoose: mongoose, // mongoose (connected)
-  expressApp: app // an express app
+  expressApp: app, // an express app
+  sessionSchema: {
+    loggedIn: {type:Boolean, default:false},
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    cart: { type: mongoose.Schema.Types.ObjectId, ref: 'Cart' }
+  },
+  userSchema: {
+    email: {type: String, required:true, unique:true},
+    password: {type: String, required:true},
+    roles: [String],
+    cart: { type: mongoose.Schema.Types.ObjectId, ref: 'Cart' }
+  }
 });
 
-// Models
+// Models //
+global.Cart = require('./models/cart.js');
 const User = accessManager.models.user;
 
+// Middleware //
+const CartMiddleware = require('./middlewares/cart-middleware.js');
+app.use(CartMiddleware);
 
+// Routes //
 app.get('/', (req,res)=>{
 
 });
